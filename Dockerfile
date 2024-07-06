d3d3ac21f7441e78d9632daeb447561b0db92a3f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2024-07-06 22:10:27 +0200

    feat: remove certbot from container
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -2,23 +2,14 @@
 
 FROM eclipse-mosquitto:2.0-openssl
 
-# install certbot and openssl (all required to enable certbot with standalone mode)
 RUN apk update \
     && apk upgrade \
     && apk add --no-cache \
-    certbot openssl
+    openssl
 
 WORKDIR /ac
  
 COPY run.sh run.sh
-
-# Scripts to renew let's encrypt certs and to upgrade certbot automatically.
-# Filenames in `/etc/periodic` must follow specific rules: https://askubuntu.com/a/611430
-# For example you cannot use "dot" and "dash" in filenames, so you must remove file extensions
-COPY certbotrenew.sh /etc/periodic/daily/certbotrenew
-# you have to make all files in /etc/periodic executables,
-# otherwise crond will skip non-exetubale files without throwing errors
-RUN chmod +x /etc/periodic/daily/certbotrenew
 
 COPY mosquitto.conf /mosquitto/config/mosquitto.conf
 COPY mosquitto-nossl.conf /mosquitto/config/mosquitto-nossl.conf
--- run.sh
+++ run.sh
@@ -1,47 +1,19 @@
 #!/bin/sh
 set -e
 
-if [ ${CERTBOT_EMAIL+x} ] && [ ${CERTBOT_DOMAIN+x} ] && [ ${CERTBOT_SERVER+x} ]; then
-  # Let's encrypt certificates
-  #Certificate is saved at: /etc/letsencrypt/live/<DOMAIN>/fullchain.pem
-  #Key is saved at:         /etc/letsencrypt/live/<DOMAIN>/privkey.pem
-
-  echo "Printing 'crontab'"
-  crontab -l
-  echo "Run crond as background process"
-  crond -b -l 0 -L /ac/crond.log
-  
-  # read env variables
-  echo "Env variables:"
-  echo "CERTBOT_EMAIL = ${CERTBOT_EMAIL}"
-  echo "CERTBOT_DOMAIN = ${CERTBOT_DOMAIN}"
-  echo "CERTBOT_SERVER = ${CERTBOT_SERVER}"
-  echo "MOSQUITTO_USERNAME = ${MOSQUITTO_USERNAME}"
-  echo "MOSQUITTO_PASSWORD = ${MOSQUITTO_PASSWORD}"
-
-  if [ -d "/etc/letsencrypt/live/${CERTBOT_DOMAIN}" ]
-  then
-    echo "Certificates already exists"
-  else
-    echo "Requesting certificates using certbot via production server"
-    certbot certonly --standalone -m "${CERTBOT_EMAIL}" --agree-tos -d ${CERTBOT_DOMAIN},www.${CERTBOT_DOMAIN} -n --server "${CERTBOT_SERVER}"
-    # staging server:
-    # certbot certonly --standalone -m "${CERTBOT_EMAIL" --agree-tos -d ${certbot_domain},www.${certbot_domain} -n --server https://acme-staging-v02.api.letsencrypt.org/directory
-  fi
-
+if [ ${MOSQUITTO_SSL_ENABLE+x} ]; then
   CERT_DIR=/etc/mosquitto/certs
   mkdir -p ${CERT_DIR}
 
-  cp "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/cert.pem" ${CERT_DIR}/cert.pem
-  cp "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/chain.pem" ${CERT_DIR}/chain.pem
-  cp "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem" ${CERT_DIR}/fullchain.pem
-  cp "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem" ${CERT_DIR}/privkey.pem
+  cp "/tls/tls.crt" ${CERT_DIR}/cert.pem
+  cp "/tls/ca.crt" ${CERT_DIR}/chain.pem
+  cp "/tls/tls.key" ${CERT_DIR}/privkey.pem
 
   # Set ownership to Mosquitto
-  chown mosquitto: ${CERT_DIR}/cert.pem ${CERT_DIR}/chain.pem ${CERT_DIR}/fullchain.pem ${CERT_DIR}/privkey.pem
+  chown mosquitto: ${CERT_DIR}/cert.pem ${CERT_DIR}/chain.pem ${CERT_DIR}/privkey.pem
 
   # Ensure permissions are restrictive
-  chmod 0600 ${CERT_DIR}/cert.pem ${CERT_DIR}/chain.pem ${CERT_DIR}/fullchain.pem ${CERT_DIR}/privkey.pem
+  chmod 0600 ${CERT_DIR}/cert.pem ${CERT_DIR}/chain.pem ${CERT_DIR}/privkey.pem
 
   ls -la ${CERT_DIR}
   ps -a
