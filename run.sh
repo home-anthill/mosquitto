71c2834993ff20b57fbd35d3f6687106b536b353
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2024-07-07 16:46:06 +0200

    chore: add logs
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 set -e
 
 if [ ${MOSQUITTO_SSL_ENABLE+x} ]; then
+  echo "SSL enabled"
   CERT_DIR=/etc/mosquitto/certs
   mkdir -p ${CERT_DIR}
 
+  echo "Copying SSL certs"
   cp "/tls/tls.crt" ${CERT_DIR}/cert.pem
   cp "/tls/ca.crt" ${CERT_DIR}/chain.pem
   cp "/tls/tls.key" ${CERT_DIR}/privkey.pem
@@ -19,6 +21,9 @@ if [ ${MOSQUITTO_SSL_ENABLE+x} ]; then
   ps -a
 
   if [ ${MOSQUITTO_USERNAME+x} ] && [ ${MOSQUITTO_PASSWORD+x} ]; then
+    echo "Configuring authentication with SSL"
+    echo "mosquitto username ${MOSQUITTO_USERNAME}"
+    echo "mosquitto password ${MOSQUITTO_USERNAME}"
     # set user and password for mosquitto
     mosquitto_passwd -b -c /etc/mosquitto/password_file ${MOSQUITTO_USERNAME} ${MOSQUITTO_PASSWORD}
     chown mosquitto:mosquitto /etc/mosquitto/password_file
@@ -26,13 +31,18 @@ if [ ${MOSQUITTO_SSL_ENABLE+x} ]; then
     # start mosquitto with user/password authentication
     mosquitto -c /mosquitto/config/mosquitto-auth.conf
   else
+    echo "Skipping authentication with SSL"
     # start mosquitto without authentication
     mosquitto -c /mosquitto/config/mosquitto.conf
   fi
 else
+  echo "SSL NOT enabled"
   ps -a
 
   if [ ${MOSQUITTO_USERNAME+x} ] && [ ${MOSQUITTO_PASSWORD+x} ]; then
+    echo "Configuring authentication without SSL"
+    echo "mosquitto username ${MOSQUITTO_USERNAME}"
+    echo "mosquitto password ${MOSQUITTO_USERNAME}"
     # set user and password for mosquitto
     mosquitto_passwd -b -c /etc/mosquitto/password_file ${MOSQUITTO_USERNAME} ${MOSQUITTO_PASSWORD}
     chown mosquitto:mosquitto /etc/mosquitto/password_file
@@ -40,6 +50,7 @@ else
     # start mosquitto with user/password authentication
     mosquitto -c /mosquitto/config/mosquitto-nossl-auth.conf
   else
+    echo "Skipping authentication without SSL"
     # start mosquitto without authentication
     mosquitto -c /mosquitto/config/mosquitto-nossl.conf
   fi
